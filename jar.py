cdd91edcfd4dfe98067cdfbe02f58afa0c0ba99a
mark <kamenskaam53@gmail.com>
2023-06-22 19:36:38 +0300

    add task

--- jar.py
+++ jar.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 import speech_recognition as sr
 import os
 import sys
@@ -9,3 +11,39 @@ def talk(words):
 
 
 talk("Hi, how can I help you?")
+
+
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+
+    try:
+        task = r.recognize_google(audio, language="ua-UA").lower()
+        print("Ви проговорили: " + task)
+    except sr.UnknownValueError:
+        talk("Я вас не зрозумів.")
+        task = command()
+
+    return task
+
+
+def make_something(task):
+    if "відкрий сайт" in task:
+        talk("Відкриваю")
+        url = "https://ituniver.com"
+        webbrowser.open(url)
+    #elif ____ in task:
+    #       talk()
+
+
+while True:
+    make_something(command())
+
+
+
+
